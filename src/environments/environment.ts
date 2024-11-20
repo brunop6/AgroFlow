14a3ab6b0c47379bbb34f0b99d64cbb2f44fefd0
brunop6 <peresbruno6@gmail.com>
2024-11-20 14:32:06 -0300

    🔧 project config

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  //Mudar para true para produção
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAzr3l46uskagL6RfSqD2CYVGuNT_N5xw8",
+    authDomain: "agroflow-c9ffa.firebaseapp.com",
+    projectId: "agroflow-c9ffa",
+    storageBucket: "agroflow-c9ffa.firebasestorage.app",
+    messagingSenderId: "654075553244",
+    appId: "1:654075553244:web:9d7111c9d3b375bc8e1b19",
+    measurementId: "G-3P78ZRQK7V"
+  }
+};
